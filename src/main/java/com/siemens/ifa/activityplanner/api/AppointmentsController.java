c4b8c39e57f968d62bb5631d277731f6a9464024
Radu Toev <radu.toev@siemens.com>
2018-08-08 15:40:07 +0300

    With simple validation

--- src/main/java/com/siemens/ifa/activityplanner/api/AppointmentsController.java
+++ src/main/java/com/siemens/ifa/activityplanner/api/AppointmentsController.java
@@ -8,6 +8,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,16 +35,23 @@ public class AppointmentsController {
 
     @RequestMapping(path = "/appointments",
             method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_JSON_VALUE)
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity create(@RequestBody Appointment appointment) {
-        try {
+        if(isValid(appointment)) {
             Appointment created = appointmentsRepo.save(appointment);
-        } catch (DataIntegrityViolationException e) {
-            System.out.println(e.getMessage());
+        } else {
             return ResponseEntity
                     .status(HttpStatus.BAD_REQUEST)
                     .body(new InfoMessage("Provided appointment is invalid"));
         }
         return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
+
+    private static boolean isValid(Appointment appointment) {
+        return !StringUtils.isEmpty(appointment.getTopic()) &&
+            !StringUtils.isEmpty(appointment.getLocation()) &&
+            appointment.getStartTime() != null &&
+            appointment.getEndTime() != null;
     }
 }
